fd9833376da2faa60cda480ec135fd94d72c01e6
nospk <nospk1706@gmail.com>
2023-02-26 19:16:56 +0700

    fix color

--- components/SearchHistory/SearchHistory.tsx
+++ components/SearchHistory/SearchHistory.tsx
@@ -1,14 +1,17 @@
 import { useState, FC } from 'react';
-import { useRouter } from 'next/router';
+
 import styles from './SearchHistory.module.css';
+import Link from 'next/link';
 const SearchHistory: FC = () => {
-    const router = useRouter();
+
     const [textInputs] = useState(['Áo Nữ', 'Thời trang nam nữ', 'ốp điện thoại', 'Quần áo trẻ em']);
     return (
         <div className={styles.search_history}>
             {textInputs.map(textInput =>
-                <span key={textInput} className={styles.text_search} onClick={() => router.push(`/search?keyword=${textInput}`)}>
-                    {textInput}
+                <span key={textInput} className={styles.text_search}>
+                    <Link href={`/search?keyword=${textInput}`} >
+                        {textInput}
+                    </Link>
                 </span>
             )}
         </div>
