69ba353019602bd1b76d201608ecd405779227a8
nospk <nospk1706@gmail.com>
2023-02-26 15:41:34 +0700

    update

--- layout/HomeLayout.tsx
+++ layout/HomeLayout.tsx
@@ -1,4 +1,3 @@
-import Head from '@/components/Head';
 import SearchHistory from '@/components/SearchHistory';
 import StickySearch from '@/components/StickSearch';
 import Footer from '@/components/Footer';
@@ -7,7 +6,6 @@ import type { ReactNode } from 'react';
 const HomeLayout = ({ children }: { children: ReactNode }) => {
     return (
         <>
-            <Head title="Trang chủ - Cây Sen Đá" />
             {/* Search Top Sticky*/}
             <StickySearch />
             {/* History Search */}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import HomeLayout from '@/layout/HomeLayout'
 import ProductCard from '@/components/ProductCard';
 import Menu from '@/components/Menu';
 import styles from './index.module.css';
-
+import Head from '@/components/Head';
 
 const Home: NextPageWithLayout = () => {
   const products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list = products.map(product => <ProductCard key={product} name='Kẹo dẻo' price={100000} sold={300} image='' />)
   return (
     <>
+      <Head title="Trang chủ - Cây Sen Đá" description="test" />
       <Menu />
       {/* Show Products */}
       <div className={styles.products}>
