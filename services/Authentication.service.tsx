63134627d13cd4531a8f1cae1b86a5a7eea4b9fe
nospk <nospk1706@gmail.com>
2023-04-17 03:55:48 +0000

    update service login

---
+++ services/Authentication.service.tsx
@@ -0,0 +1,22 @@
+import API from "@/uitls/api";
+
+const login = async ({username, password}: {username:string, password:string}) =>{
+    let user = await API.GET({
+      path: '/authentication/login',
+      data: { username, password },
+      auth: false,
+    });
+    return user
+}
+const checkToken = async (token:string)=>{
+    let check = await API.GET({
+        path: '/authentication/check-token',
+        data: token,
+        auth: false
+    })
+    return check
+}
+const Auth = {
+    login, checkToken
+}
+export default Auth
