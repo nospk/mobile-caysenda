aabf7533fb814551e4fc888bea7720c19893adfa
nospk <nospk1706@gmail.com>
2023-05-05 09:47:27 +0000

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+export default function Custom404() {
+  const router = useRouter();
+  return (
+    <>
+      <div className="h-screen text-center flex flex-col items-center justify-center">
+        <div>
+          <h1 className="inline-block border-r-2 pr-5 font-semibold text-2xl leading-10">
+            404
+          </h1>
+          <h2 className="inline-block pl-5 text-sm">
+            This page could not be found.
+          </h2>
+        </div>
+        <div className="mt-5">
+          <button
+            className="bg-[#f66d09] px-2 py-1 rounded-full text-white"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            Trang Chủ
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
